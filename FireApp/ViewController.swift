519002c2684e4f723b36906a6e4082a29cabf3eb
ayukin <uh1ohdpwgm@gmail.com>
2021-06-01 04:55:05 +0900

    Initial Commit

---
+++ FireApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireApp
+//
+//  Created by Ayuki Nishioka on 2021/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
