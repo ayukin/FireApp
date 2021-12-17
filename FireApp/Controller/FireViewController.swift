5ae359f68cf5ddef766c4ea79d0d2312f9daac15
A.Nishioka(720) <uf1ohdpwgm@gmail.com>
2021-12-17 19:27:09 +0900

    追加修正

--- FireApp/Controller/FireViewController.swift
+++ FireApp/Controller/FireViewController.swift
@@ -14,23 +14,24 @@ class FireViewController: UIViewController {
     
     private let disposeBag = DisposeBag()
     
-    // 達成金額label
-    @IBOutlet weak var wrapView: UIView!
-    @IBOutlet weak var fireMoneyLabel: UILabel!
-    @IBOutlet weak var sideFireMoneyLabel: UILabel!
-    @IBOutlet weak var goalMoneyLabel: UILabel!
+    @IBOutlet private weak var wrapView: UIView!
     
-    // 達成コメントlabel
-    @IBOutlet weak var fireAchievementLabel: UILabel!
-    @IBOutlet weak var sideFireAchievementlabel: UILabel!
-    @IBOutlet weak var goalAchievementLabel: UILabel!
+    // 達成ラベル
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var moneyLabel: UILabel!
+    @IBOutlet private weak var achievementLabel: UILabel!
+    
+    // 切替ボタン
+    @IBOutlet private weak var fireChangeButton: UIButton!
+    @IBOutlet private weak var sideFireChangeButton: UIButton!
+    @IBOutlet private weak var goalChangeButton: UIButton!
     
     // spreadsheetView
-    @IBOutlet weak var spreadsheetView: SpreadsheetView!
+    @IBOutlet private weak var spreadsheetView: SpreadsheetView!
     
     // ナビバーボタン
-    @IBOutlet weak var recordButton: UIBarButtonItem!
-    @IBOutlet weak var graphButton: UIBarButtonItem!
+    @IBOutlet private weak var recordButton: UIBarButtonItem!
+    @IBOutlet private weak var graphButton: UIBarButtonItem!
     
     private let data = ["年数", "年齢", "年間投資額", "投資元本", "年利", "単年利益", "累計利益", "合計資産", "前年比"]
         
@@ -48,10 +49,43 @@ class FireViewController: UIViewController {
     
     private func setup() {
         // wrapViewのセットアップ
-        wrapView.layer.masksToBounds = true
         wrapView.layer.cornerRadius = 10
         wrapView.layer.borderColor = UIColor.black.cgColor
         wrapView.layer.borderWidth = 0.5
+        wrapView.layer.shadowOpacity = 0.3
+        wrapView.layer.shadowRadius = 1
+        wrapView.layer.shadowColor = UIColor.black.cgColor
+        wrapView.layer.shadowOffset = CGSize(width: 2, height: 2)
+        
+        // 達成ラベル
+        titleLabel.text = "FIREに必要な金額"
+        moneyLabel.text = "00,000,000円"
+        achievementLabel.text = "○○年後の●●歳にFIREできます。"
+        
+        // fireChangeButtonのセットアップ
+        fireChangeButton.backgroundColor = UIColor(named: "custom_Red")
+        fireChangeButton.layer.cornerRadius = 10
+        fireChangeButton.layer.shadowOpacity = 0.3
+        fireChangeButton.layer.shadowRadius = 1
+        fireChangeButton.layer.shadowColor = UIColor.black.cgColor
+        fireChangeButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        
+        // sideFireChangeButtonのセットアップ
+        sideFireChangeButton.backgroundColor = UIColor.systemGray
+        sideFireChangeButton.layer.cornerRadius = 10
+        sideFireChangeButton.layer.shadowOpacity = 0.3
+        sideFireChangeButton.layer.shadowRadius = 1
+        sideFireChangeButton.layer.shadowColor = UIColor.black.cgColor
+        sideFireChangeButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        
+        // goalChangeButtonのセットアップ
+        goalChangeButton.backgroundColor = UIColor.systemGray
+        goalChangeButton.layer.cornerRadius = 10
+        goalChangeButton.layer.shadowOpacity = 0.3
+        goalChangeButton.layer.shadowRadius = 1
+        goalChangeButton.layer.shadowColor = UIColor.black.cgColor
+        goalChangeButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        
         // spreadsheetViewのセットアップ
         spreadsheetView.register(SpreadsheetCell.self, forCellWithReuseIdentifier: SpreadsheetCell.identifier)
         spreadsheetView.gridStyle = .solid(width: 1, color: .black)
@@ -77,6 +111,42 @@ class FireViewController: UIViewController {
             }
             .disposed(by: disposeBag)
         
+        fireChangeButton.rx.tap
+            .asDriver()
+            .drive { [weak self] _ in
+                self?.titleLabel.text = "FIREに必要な金額"
+                self?.moneyLabel.text = "00,000,000円"
+                self?.achievementLabel.text = "○○年後の●●歳にFIREできます。"
+                self?.fireChangeButton.backgroundColor = UIColor(named: "custom_Red")
+                self?.sideFireChangeButton.backgroundColor = UIColor.systemGray
+                self?.goalChangeButton.backgroundColor = UIColor.systemGray
+            }
+            .disposed(by: disposeBag)
+        
+        sideFireChangeButton.rx.tap
+            .asDriver()
+            .drive { [weak self] _ in
+                self?.titleLabel.text = "サイドFIREに必要な金額"
+                self?.moneyLabel.text = "00,000,000円"
+                self?.achievementLabel.text = "○○年後の●●歳にサイドFIREできます。"
+                self?.fireChangeButton.backgroundColor = UIColor.systemGray
+                self?.sideFireChangeButton.backgroundColor = UIColor(named: "custom_Red")
+                self?.goalChangeButton.backgroundColor = UIColor.systemGray
+            }
+            .disposed(by: disposeBag)
+        
+        goalChangeButton.rx.tap
+            .asDriver()
+            .drive { [weak self] _ in
+                self?.titleLabel.text = "目標金額"
+                self?.moneyLabel.text = "00,000,000円"
+                self?.achievementLabel.text = "○○年後の●●歳に目標金額に到達できます。"
+                self?.fireChangeButton.backgroundColor = UIColor.systemGray
+                self?.sideFireChangeButton.backgroundColor = UIColor.systemGray
+                self?.goalChangeButton.backgroundColor = UIColor(named: "custom_Red")
+            }
+            .disposed(by: disposeBag)
+                
     }
     
     @objc func dismissKeyboard() {
@@ -91,19 +161,20 @@ extension FireViewController: SpreadsheetViewDelegate {
     }
 }
 
-extension FireViewController: SpreadsheetViewDataSource {
+extension FireViewController: SpreadsheetViewDataSource {    
+    
     // 列数
     func numberOfColumns(in spreadsheetView: SpreadsheetView) -> Int {
-        return 50
+        return data.count
     }
     // 行数
     func numberOfRows(in spreadsheetView: SpreadsheetView) -> Int {
-        return data.count
+        return 50
     }
     // セルの幅
     func spreadsheetView(_ spreadsheetView: SpreadsheetView, widthForColumn column: Int) -> CGFloat {
-        if column == 0 {
-            return 100
+        if column == 0 || column == 1 || column == 4 {
+            return 75
         } else {
             return 150
         }
@@ -115,52 +186,108 @@ extension FireViewController: SpreadsheetViewDataSource {
     // 列ヘッダーの固定
     func frozenColumns(in spreadsheetView: SpreadsheetView) -> Int {
         return 1
-    }    
+    }
+    // 行ヘッダーの固定
+    func frozenRows(in spreadsheetView: SpreadsheetView) -> Int {
+        return 1
+    }
     // セルの生成
     func spreadsheetView(_ spreadsheetView: SpreadsheetView, cellForItemAt indexPath: IndexPath) -> Cell? {
         guard let cell = spreadsheetView.dequeueReusableCell(withReuseIdentifier: SpreadsheetCell.identifier, for: indexPath) as? SpreadsheetCell else {
             return Cell()
         }
         
-        if indexPath.section == 0 {
-            cell.setup(with: data[indexPath.row])
-            cell.backgroundColor = UIColor(named: "custom_Pink")
-            cell.textField.isUserInteractionEnabled = false
-        } else {
-            cell.backgroundColor = UIColor.white
-            cell.textField.isUserInteractionEnabled = false
-            
-            switch indexPath.row {
-            case 0:
-                cell.setup(with: "\(indexPath.section)")
-                
-            case 1:
-                let age = 30
-                cell.setup(with: "\(age+Int(indexPath.section))歳")
-                
-            case 2:
-                cell.setup(with: "¥1800000円")
+        // セルの編集Bool値
+        switch indexPath.section {
+        case 2:
+            if indexPath.row == 0 {
+                cell.textField.isUserInteractionEnabled = false
+            } else {
                 cell.textField.isUserInteractionEnabled = true
+            }
+        default:
+            cell.textField.isUserInteractionEnabled = false
+        }
+        
+        // セルの配色
+        switch indexPath.section {
+        case 2:
+            if indexPath.row == 0 {
+                cell.backgroundColor = UIColor(named: "custom_Pink")
+            } else {
                 cell.backgroundColor = UIColor.systemGray5
-                
-            case 3:
+            }
+        default:
+            if indexPath.row == 0 {
+                cell.backgroundColor = UIColor(named: "custom_Pink")
+            } else {
+                cell.backgroundColor = UIColor.white
+            }
+        }
+        
+        // セルの値
+        switch indexPath.section {
+        case 0:
+            if indexPath.row == 0 {
+                cell.setup(with: data[indexPath.section])
+            } else {
+                cell.setup(with: "\(indexPath.row)")
+            }
+        case 1:
+            if indexPath.row == 0 {
+                cell.setup(with: data[indexPath.section])
+            } else {
+                // ViewModelから受け取る
+                cell.setup(with: "\(30+Int(indexPath.row))歳")
+            }
+        case 2:
+            if indexPath.row == 0 {
+                cell.setup(with: data[indexPath.section])
+            } else {
+                // ViewModelから受け取る
                 cell.setup(with: "¥1800000円")
-                
-            case 4:
+            }
+        case 3:
+            if indexPath.row == 0 {
+                cell.setup(with: data[indexPath.section])
+            } else {
+                // ViewModelから受け取る
+                cell.setup(with: "¥1800000円")
+            }
+        case 4:
+            if indexPath.row == 0 {
+                cell.setup(with: data[indexPath.section])
+            } else {
+                // ViewModelから受け取る
                 cell.setup(with: "5％")
-                
-            case 5:
+            }
+        case 5:
+            if indexPath.row == 0 {
+                cell.setup(with: data[indexPath.section])
+            } else {
+                // ViewModelから受け取る
                 cell.setup(with: "¥1800000円")
-                
-            case 6:
+            }
+        case 6:
+            if indexPath.row == 0 {
+                cell.setup(with: data[indexPath.section])
+            } else {
+                // ViewModelから受け取る
                 cell.setup(with: "¥1800000円")
-                
-            case 7:
+            }
+        case 7:
+            if indexPath.row == 0 {
+                cell.setup(with: data[indexPath.section])
+            } else {
+                // ViewModelから受け取る
                 cell.setup(with: "¥1800000円")
-
-            default:
+            }
+        default:
+            if indexPath.row == 0 {
+                cell.setup(with: data[indexPath.section])
+            } else {
+                // ViewModelから受け取る
                 cell.setup(with: "¥1800000円")
-                                
             }
         }
         return cell
--- FireApp/View/SpreadsheetCell.swift
+++ FireApp/View/SpreadsheetCell.swift
@@ -7,40 +7,55 @@
 
 import UIKit
 import SpreadsheetView
+import RxSwift
+import RxCocoa
 
 class SpreadsheetCell: Cell {
+    
+    private let disposeBag = DisposeBag()
     
     static let identifier = "SpreadsheetTextCell"
     let textField = UITextField()
     
     public func setup(with text: String) {
-        textField.delegate = self
         textField.text = text
         textField.textAlignment = .center
+        textField.keyboardType = .numberPad
         contentView.addSubview(textField)
+        
+        // textFieldの値が変更されるたびに処理が走る
+        textField.rx.controlEvent(.editingChanged).asDriver()
+            .drive(onNext: { _ in
+                print("editingChanged")
+            })
+            .disposed(by: disposeBag)
+        
+        // キーボードが閉じた時に処理が走る
+        textField.rx.controlEvent(.editingDidEnd).asDriver()
+            .drive(onNext: { _ in
+                print("editingDidEnd")
+            })
+            .disposed(by: disposeBag)
+        
+        // よくわからない
+        textField.rx.controlEvent(.editingDidEndOnExit).asDriver()
+            .drive(onNext: { _ in
+                
+                print("editingDidEndOnExit")
+            })
+            .disposed(by: disposeBag)
+        
+        // よくわからない
+        textField.rx.controlEvent(.valueChanged).asDriver()
+            .drive(onNext: { _ in
+                print("valueChanged")
+            })
+            .disposed(by: disposeBag)
+        
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         textField.frame = contentView.bounds
     }
-}
-
-extension SpreadsheetCell: UITextFieldDelegate {
-    // 入力開始時の処理
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-    }
-    // textFieldの内容をリアルタイムで反映させる
-    func textFieldDidChangeSelection(_ textField: UITextField) {
-    }
-    // リターンキーを押したときキーボードが閉じる
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    // 入力終了時の処理（フォーカスがはずれる）
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        textField.resignFirstResponder()
-    }
-    
 }
